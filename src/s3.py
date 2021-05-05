c86e310371fd14df0c862d255f7b5bc9844a1e49
Sara Ho <saraho2021@u.northwestern.edu>
2021-05-04 23:41:16 -0500

    clean s3 file

--- src/s3.py
+++ src/s3.py
@@ -61,32 +61,6 @@ def upload_to_s3_pandas(local_path, s3path, sep=';'):
             logger.info(f'Data uploaded from {local_path} to {s3path}')
 
 
-# def download_files_from_s3(local_path, s3path):
-
-#     s3bucket, s3_just_path = parse_s3(s3path)
-
-#     s3 = boto3.resource("s3")
-#     bucket = s3.Bucket(s3bucket)
-
-#     try:
-#         bucket.download_file(s3_just_path, local_path)
-#     except botocore.exceptions.NoCredentialsError:
-#         logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
-#     else:
-#         logger.info('Data downloaded from %s to %s', s3path, local_path)
-
-
-# def download_from_s3_pandas(local_path, s3path, sep=';'):
-
-#     try:
-#         df = pd.read_csv(s3path, sep=sep)
-#     except botocore.exceptions.NoCredentialsError:
-#         logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
-#     else:
-#         df.to_csv(local_path, sep=sep)
-#         logger.info('Data uploaded from %s to %s', local_path, s3path)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--sep',
@@ -96,20 +70,12 @@ if __name__ == '__main__':
                         help="load data?")
     parser.add_argument('--pandas', default=False, action='store_true',
                         help="If used, will load data via pandas")
-    # parser.add_argument('--download', default=False, action='store_true',
-    #                     help="If used, will load data via pandas")
     parser.add_argument('--s3path', default='s3://2021-msia-423-ho-sara',
                         help="If used, will load data via pandas")
     parser.add_argument('--local_path', default='./data',
                         help="Where to load data to in S3")
     args = parser.parse_args()
 
-    # if args.download:
-    #     if args.pandas:
-    #         download_from_s3_pandas(args.local_path, args.s3path, args.sep)
-    #     else:
-    #         download_files_from_s3(args.local_path, args.s3path)
-    # else:
     if args.load:
         news = load_news()
         load_wiki(news)
