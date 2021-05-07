0f5fcfca8c9ed8600a81234b7e3adb1d75b99f0d
Sara Ho <saraho2021@u.northwestern.edu>
2021-05-06 21:13:41 -0500

    simplifications and specify s3 filepath

--- src/s3.py
+++ src/s3.py
@@ -18,18 +18,12 @@ logging.getLogger("asyncio").setLevel(logging.ERROR)
 logging.getLogger("aiobotocore").setLevel(logging.ERROR)
 logging.getLogger("s3fs").setLevel(logging.ERROR)
 
-
 logger = logging.getLogger('s3')
-
-
-def parse_s3(s3path):
-    s3bucket = s3path.replace('s3://', '')
-    return s3bucket
 
 
 def upload_files_to_s3(local_path, s3path):
 
-    s3bucket = parse_s3(s3path)
+    s3bucket = s3path.replace('s3://', '')
 
     s3 = boto3.resource("s3")
     bucket = s3.Bucket(s3bucket)
@@ -38,47 +32,26 @@ def upload_files_to_s3(local_path, s3path):
     files = [file for file in files if '.csv' in file]
     for file in files:
         try:
-            bucket.upload_file(f'{local_path}/{file}', file)
+            bucket.upload_file(f'{local_path}/{file}', f'raw/{file}')
         except botocore.exceptions.NoCredentialsError:
             logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
         else:
-            logger.info(f'Data uploaded from {local_path} to {s3path}')
-
-
-# def upload_to_s3_pandas(local_path, s3path):
-
-#     files = os.listdir(local_path)
-#     for file in files:
-#         df = pd.read_csv(local_path + FileExistsError)
-
-#         try:
-#             df.to_csv(s3path + file)
-#         except botocore.exceptions.NoCredentialsError:
-#             logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
-#         else:
-#             logger.info(f'Data uploaded from {local_path} to {s3path}')
+            logger.info(f'Data uploaded from {local_path}/{file} to {s3path}/raw/{file}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sep',
-                        default=';',
-                        help="CSV separator if using pandas")
     parser.add_argument('--load', default=False,
-                        help="load data?")
-    parser.add_argument('--pandas', default=False, action='store_true',
-                        help="If used, will load data via pandas")
+                        help="Load new data into local folder via API")
     parser.add_argument('--s3path', default='s3://2021-msia-423-ho-sara',
-                        help="If used, will load data via pandas")
+                        help="If used, will load data to s3")
     parser.add_argument('--local_path', default='./data',
                         help="Where to load data to in S3")
     args = parser.parse_args()
 
     if args.load:
+        # this will save csv files into local path with new daily data
         news = load_news()
         load_wiki(news)
 
-    if args.pandas:
-        upload_to_s3_pandas(args.local_path, args.s3path, args.sep)
-    else:
-        upload_files_to_s3(args.local_path, args.s3path)
+    upload_files_to_s3(args.local_path, args.s3path)
