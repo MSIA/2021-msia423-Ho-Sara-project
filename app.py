be9216d6f4232c1548a1425bc44a03b30c3c28e5
Sara Ho <saraho2021@u.northwestern.edu>
2021-05-06 21:14:47 -0500

    flask app is working

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 import traceback
 import logging.config
+
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 
+# Initialize the database session
+from src.add_records import Wiki, News, WikiNewsManager
+
 # Initialize the Flask application
-app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
+app = Flask(__name__, 
+            template_folder="app/templates", 
+            static_folder="app/static")
 
 # Configure flask app from flask_config.py
 app.config.from_pyfile('config/flaskconfig.py')
@@ -13,48 +20,34 @@ app.config.from_pyfile('config/flaskconfig.py')
 # up the logger (e.g. config/logging/local.conf)
 logging.config.fileConfig(app.config["LOGGING_CONFIG"])
 logger = logging.getLogger(app.config["APP_NAME"])
+db = 'sqlite:///data/entries.db'
 logger.debug('Web app log')
 
-# Initialize the database session
-from src.add_songs import Tracks, TrackManager
-track_manager = TrackManager(app)
+manager = WikiNewsManager(engine_string=db)
 
 @app.route('/')
 def index():
-    """Main view that lists songs in the database.
+    """Main view that lists some news headlines
+    and associated wikipedia articles.
 
-    Create view into index page that uses data queried from Track database and
-    inserts it into the msiapp/templates/index.html template.
+    Create view into index page that uses data queried from entries database
+    inserts it into the ./templates/index.html template.
 
     Returns: rendered html template
 
     """
 
     try:
-        tracks = track_manager.session.query(Tracks).limit(app.config["MAX_ROWS_SHOW"]).all()
+        wiki_entities = manager.session.query(Wiki).all()
         logger.debug("Index page accessed")
-        return render_template('index.html', tracks=tracks)
+        return render_template('index.html', wiki_entities=wiki_entities)
     except:
         traceback.print_exc()
-        logger.warning("Not able to display tracks, error page returned")
-        return render_template('error.html')
-
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    """View that process a POST with new song input
-
-    :return: redirect to index page
-    """
-
-    try:
-        track_manager.add_track(artist=request.form['artist'], album=request.form['album'], title=request.form['title'])
-        logger.info("New song added: %s by %s", request.form['title'], request.form['artist'])
-        return redirect(url_for('index'))
-    except:
-        logger.warning("Not able to display tracks, error page returned")
+        logger.warning("Not able to display wikinews, error page returned")
         return render_template('error.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=app.config["DEBUG"], port=app.config["PORT"], host=app.config["HOST"])
+    app.run(debug=app.config["DEBUG"], 
+            port=app.config["PORT"], 
+            host=app.config["HOST"])
