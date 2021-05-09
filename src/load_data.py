bae0311bf5daee846d2c96faba2a35056f3e7862
Sara Ho <saraho2021@u.northwestern.edu>
2021-05-09 14:48:34 -0500

    major changes to exception handling, logging, docstrings, parameters.

--- src/load_data.py
+++ src/load_data.py
@@ -6,30 +6,31 @@ import signal
 import spacy
 import pandas as pd
 
-from src.api_helpers import *
+from src.api_helpers import wiki_query, wiki_pagecontent, news_top
 
 logging.config.fileConfig("config/logging/local.conf",
                           disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-class TimeoutException(Exception):
-    pass
-def timeout_handler(signum, frame):
-    raise TimeoutException
-signal.signal(signal.SIGALRM, timeout_handler)
 
-
-def load_news():
+def load_news(NEWS_API_KEY, directory='./data'):
     """Create pandas dataframe from daily headlines from News API
+
     Args:
-        None
+        NEWS_API_KEY: `str` generated from https://newsapi.org/
+        directory: local directory to save .csv file
 
-    Returns: obj `pandas.DataFrame`
-
+    Returns: 
+        obj `pandas.DataFrame`
     """
     today = date.today().strftime("%b-%d-%Y")
     logger.info('loading news from API')
-    data = news_top()
+
+    try:
+        data = news_top(NEWS_API_KEY)
+    except Exception as e:
+        logger.error(f'Error {e} when retrieving headlines')
+
     news = []
     for article in data['articles']:
 
@@ -44,13 +45,40 @@ def load_news():
     news_table = pd.DataFrame(news).reset_index()
     news_table.columns = ['news_id', 'news']
 
-    news_table.to_csv(f'./data/{today}-news-entries', index=False)
-    logger.info(f"----Saved to csv data for {len(news_table)} headlines...")
+    try:
+        news_table.to_csv(f'{directory}/{today}-news-entries.csv', index=False)
+        logger.info("----Saved %i headlines to csv", len(news_table))
+    except FileNotFoundError as e:
+        logger.warning('----Error in saving csv locally; Still returning DataFrame obj')
+        logger.warning(e)
+
     return news_table
 
 
-def load_wiki(news_table, timeout=300):
+def news2entities(news):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(news)
+
+    entities = []
+    labels = []
+    for ent in doc.ents:
+        if (ent.label_ in ['PERSON', 'FAC', 'ORG', 'NORP', 'PRODUCT']) and (ent.text not in entities):
+            if ent.label_ == 'PERSON':
+                labels.append('PERSON')
+            elif ent.label_ == 'NORP':
+                labels.append('GROUP')
+            elif ent.label_ == 'ORG':
+                labels.append('ORGANIZATION')
+            else:
+                labels.append('')
+            entities.append(ent.text)
+
+    return entities, labels
+
+
+def load_wiki(news_table, directory='./data', n_results=3, timeout=300):
     """Match news with wikipedia articles
+
     Args:
         news_table: pandas dataframe with news headlines
         timeout: `int` number of seconds to wait for a wiki API query
@@ -58,76 +86,57 @@ def load_wiki(news_table, timeout=300):
     Returns: obj `pandas.DataFrame`
 
     """
+
     today = date.today().strftime("%b-%d-%Y")
     nlp = spacy.load("en_core_web_sm")
-
     logger.info('matching news with wiki entries from Wikipedia API')
+
     table_data = []
     for _, row in news_table.iterrows():
         news_id, news = row
 
-        logger.info(f"----Processing '{news[0:25]}...'")
+        logger.info("----Processing '%s...'", news[0:25])
 
-        # start to break query into named entities
-        doc = nlp(news)
-
-        entities = []
-        labels = []
-        for ent in doc.ents:
-            if (ent.label_ in ['PERSON', 'FAC', 'ORG', 'NORP', 'PRODUCT']) and (ent.text not in entities):
-                if ent.label_ == 'PERSON':
-                    labels.append('PERSON')
-                elif ent.label_ == 'NORP':
-                    labels.append('GROUP')
-                elif ent.label_ == 'ORG':
-                    labels.append('ORGANIZATION')
-                else:
-                    labels.append('')
-
-                entities.append(ent.text)
-
-        signal.alarm(timeout)
+        entities, labels = news2entities(news)
         for ent, label in zip(entities, labels):
-            try:
-                articledata = wiki_query(ent)
-                search_results = articledata['query']['search']
+            articledata = wiki_query(ent)
+            search_results = articledata['query']['search']
 
-                for result in search_results[0:3]:  # take the top 3
-                    title = result['title']
+            for result in search_results[0:n_results]:  # take the top 3
+                title = result['title']
 
-                    info = wiki_pageinfo(title)
-                    info = list(info['query']['pages'].values())[0]
-                    categories = [kv['title'] for kv in info['categories'] if 'births' not in kv['title']]
+                info = wiki_pagecontent(title)
+                try:
+                    categories = info['categories']
+                    categories = [kv['title'] for kv in categories if 'births' not in kv['title']]
+                except KeyError:
+                    pass
 
-                    # ignore diambiguation pages
-                    if 'Category:Disambiguation pages' in categories:
-                        pass
+                if 'Category:Disambiguation pages' in categories:
+                    pass
+                else:
+
+                    if 'thumbnail' in info:
+                        image = info['thumbnail']['source']
                     else:
-                        content = list(wiki_pagecontent(title)['query']['pages'].values())[0]
+                        image = ''
 
-                        if 'thumbnail' in content:
-                            image = content['thumbnail']['source']
-                        else:
-                            image = ''
-
-                        table_data.append({'news_id': news_id,
-                                           'entity': ent,
-                                           'label': label,
-                                           'title': info['title'],
-                                           'category': categories[0],
-                                           'revised': info['touched'],
-                                           'url': info['fullurl'],
-                                           'wiki': content['extract'],
-                                           'image': image})
-
-            except(IndexError):
-                continue
-
-            except TimeoutException:
-                logger.warning('Wiki API taking too long. Consider exiting and trying at a different time.')
-                continue
+                    table_data.append({'news_id': news_id,
+                                       'entity': ent,
+                                       'label': label,
+                                       'title': info['title'],
+                                       'category': categories[0],
+                                       'revised': info['touched'],
+                                       'url': info['fullurl'],
+                                       'wiki': info['extract'],
+                                       'image': image})
 
     table = pd.DataFrame(table_data)
-    table.to_csv(f'./data/{today}-wiki-entries.csv',
-                 index=False)
+    try:
+        table.to_csv(f'{directory}/{today}-wiki-entries.csv', index=False)
+        logger.info("----Saved %i wiki entries to csv", len(table))
+    except FileNotFoundError as e:
+        logger.warning('----Error in saving csv locally; still returning DataFrame obj')
+        logger.warning(e)
+
     return table
