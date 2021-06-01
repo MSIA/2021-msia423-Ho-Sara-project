9ac411e70dd58a9e1184b2b7e234e87939c4537e
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-01 00:23:58 -0500

    simplify run.py to work with yaml and makefile

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from datetime import date
 import unicodedata
 import logging
 import logging.config
+import yaml
 
 import pandas as pd
 import boto3
@@ -60,7 +61,7 @@ def remove_accents(s):
 def render_text(text, entities):
     entity_locs = []
     for ent in entities:
-        if '(organization)' in ent:
+        if ' (organization)' in ent:
             ent = ent.replace(' (organization)', '')
         start = text.find(ent)
         end = start + len(ent)
@@ -75,79 +76,142 @@ def render_text(text, entities):
     return displacy.render(doc, style="ent", jupyter=False, manual=True, options=options)
 
 
-def ingest(df):
-    tm = WikiNewsManager(engine_string=args.engine_string)
-    df = df.fillna('')
+def ingest_wiki(wiki_df, engine_string):
+    """ ingest wiki dataframe """
 
-    df['title'] = df['title'].apply(remove_accents)
-    # when accents are removed, the primary keys may no longer be unique
-    df = df.drop_duplicates(['news_id', 'entity', 'title'])
-
-    wiki_df = df[['date', 'news_id', 'title', 'wiki', 'wiki_url', 'wiki_image']].drop_duplicates()
-
+    tm = WikiNewsManager(engine_string=engine_string)
     for _, row in wiki_df.iterrows():
         date, news_id, title, wiki, url, image = row
         tm.add_wiki(date, news_id, title, wiki, url, image)
     logger.info(f"{len(wiki_df)} rows added to 'wiki' table")
+    tm.close()
 
-    news_df = df[['date', 'news_id', 'news', 'news_image', 'news_url']].drop_duplicates()
 
+def ingest_news(news_df, df, engine_string):
+    """ ingest news dataframe """
+
+    tm = WikiNewsManager(engine_string=engine_string)
     for _, row in news_df.iterrows():
         date, news_id, news, image, url = row
         entities = df.loc[df['news_id'] == news_id, 'entity'].values
         news_dis = render_text(news, entities)
         tm.add_news(date, news_id, news, news_dis, image, url)
     logger.info(f"{len(news_df)} rows  added to 'news' table")
-
     tm.close()
+
+
+def ingest(file_path, engine_string):
+    """after data is joined and filtered, ingest to database
+
+    Args:
+        df (obj `pandas.DataFrame`): output from filter_algo()
+    """
+    df = pd.read_csv(file_path)
+    df = df.fillna('')
+
+    # when accents are removed, the primary keys may no longer be unique
+    df['title'] = df['title'].apply(remove_accents)
+    df = df.drop_duplicates(['news_id', 'entity', 'title'])
+
+    wiki_df = df[['date', 'news_id', 'title', 'wiki', 'wiki_url', 'wiki_image']].drop_duplicates()
+    ingest_wiki(wiki_df, engine_string)
+
+    news_df = df[['date', 'news_id', 'news', 'news_image', 'news_url']].drop_duplicates()
+    ingest_news(news_df, df, engine_string)
 
 
 if __name__ == '__main__':
 
-    # Add parsers for both creating a database and adding entries to it
     parser = argparse.ArgumentParser(description="Create and/or add data to database")
-    subparsers = parser.add_subparsers(dest='subparser_name')
-
-    # Sub-parser for creating a database
-    sb_create = subparsers.add_parser('create_db', description="Create database")
-    sb_create.add_argument("--engine_string", default='sqlite:///data/entries.db',
-                           help="SQLAlchemy connection URI for database")
-
-    # Sub-parser for ingesting new data
-    sb_ingest = subparsers.add_parser('ingest', description="Add data to database")
-    sb_ingest.add_argument("--engine_string", default='sqlite:///data/entries.db',
-                           help="SQLAlchemy connection URI for database")
-    sb_ingest.add_argument("--local_path", default='./data/sample',
-                           help="Filepath for local data for database to ingest")
-
-    # Sub-parser to load new data into local ./data folder
-    sb_load = subparsers.add_parser('load_new', description="Query new data from APIs and save locally")
-
-    # Sub-parser to load data from local path into s3
-    sb_s3 = subparsers.add_parser('load_s3', description="Describe where local data is")
-    sb_s3.add_argument('--s3path', default='s3://2021-msia-423-ho-sara',
-                       help="If used, will load data to s3")
-    sb_s3.add_argument('--local_path', default='./data/sample',
-                       help="Where to load data to in S3")
+    parser.add_argument('step', help='Which step to run', choices=['load_news', 'load_wiki', 'filter', 'create_db', 'ingest'])
+    parser.add_argument('--input', '-i', default=None, help='Path to input data')
+    parser.add_argument('--config', help='Path to configuration file')
+    parser.add_argument('--output', '-o', default=None, help='Path to save output CSV (optional, default = None)')
+    parser.add_argument("--engine_string", default='sqlite:///data/entries.db',
+                        help="SQLAlchemy connection URI for database")
 
     args = parser.parse_args()
-    sp_used = args.subparser_name
 
-    if sp_used == 'create_db':
+    if args.config is not None:
+        with open(args.config, "r") as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+
+        logger.info("Configuration file loaded from %s" % args.config)
+
+    if args.input is not None:
+        input = pd.read_csv(args.input)
+        logger.info('Input data loaded from %s', args.input)
+
+    if args.step == 'load_news':
+        output = load_news(NEWS_API_KEY)
+
+    elif args.step == 'load_wiki':
+        output = load_wiki(args.input)
+
+    elif args.step == 'filter':
+        output = filter_data('data/daily')
+
+    elif args.step == 'create_db':
         create_db(args.engine_string)
 
-    elif sp_used == 'ingest':
-        df = join_data('./data/sample')
-        logger.info(df.columns)
-        ingest(df)
+    elif args.step == 'ingest':
+        ingest(args.input, args.engine_string)
 
-    elif sp_used == 'load_new':
-        # by default, this will save new csv files into ./data
-        news = load_news(NEWS_API_KEY)
-        load_wiki(news, n_results=1)
+    # else:
+    #     test.run_tests(args)
 
-    elif sp_used == 'load_s3':
-        upload_files_to_s3(args.local_path, args.s3path)
+    if args.output is not None:
+        output.to_csv(args.output, index=False)
+        logger.info("Output saved to %s" % args.output)
 
-    else:
-        parser.print_help()
+    # # Add parsers for both creating a database and adding entries to it
+    # parser = argparse.ArgumentParser(description="Create and/or add data to database")
+    # subparsers = parser.add_subparsers(dest='subparser_name')
+
+    # # Sub-parser for creating a database
+    # sb_create = subparsers.add_parser('create_db', description="Create database")
+    # sb_create.add_argument("--engine_string", default='sqlite:///data/entries.db',
+    #                        help="SQLAlchemy connection URI for database")
+
+    # # Sub-parser for ingesting new data
+    # sb_ingest = subparsers.add_parser('ingest', description="Add data to database")
+    # sb_ingest.add_argument("--engine_string", default='sqlite:///data/entries.db',
+    #                        help="SQLAlchemy connection URI for database")
+    # sb_ingest.add_argument("--local_path", default='./data/sample',
+    #                        help="Filepath for local data for database to ingest")
+
+    # # Sub-parser to load new data into local ./data folder
+    # sb_load = subparsers.add_parser('load_new', description="Query new data from APIs and save locally")
+    # sb_load.add_argument('--local_path', default='./data/',
+    #                      help="Load daily local data")
+
+    # # Sub-parser to load data from local path into s3
+    # sb_s3 = subparsers.add_parser('load_s3', description="Describe where local data is")
+    # sb_s3.add_argument('--s3path', default='s3://2021-msia-423-ho-sara',
+    #                    help="If used, will load data to s3")
+    # sb_s3.add_argument('--local_path', default='./data/sample',
+    #                    help="Where to load data to in S3")
+
+    # args = parser.parse_args()
+    # sp_used = args.subparser_name
+
+    # if sp_used == 'create_db':
+    #     create_db(args.engine_string)
+
+    # elif sp_used == 'ingest':
+    #     engine_string = args.engine_string
+    #     df = join_data(args.local_path)
+    #     df = filter_data(df)
+    #     logger.info(df.columns)
+    #     ingest(df, engine_string)
+
+    # elif sp_used == 'load_new':
+    #     # by default, this will save new csv files into ./data
+    #     news = load_news(NEWS_API_KEY, args.local_path)
+    #     load_wiki(news, n_results=1)
+
+    # elif sp_used == 'load_s3':
+    #     upload_files_to_s3(args.local_path, args.s3path)
+
+    # else:
+    #     parser.print_help()
