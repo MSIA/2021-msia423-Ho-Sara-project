17b43cfee146f8d1a34ecd8ca39bc5a79f536462
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-01 00:45:54 -0500

    modularize ingest functions

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ import spacy
 from spacy import displacy
 
 from src.load_data import load_wiki, load_news
-from src.add_entries import WikiNewsManager, create_db
+from src.ingest import *
 from src.filter_algo import filter_data, join_data
 
 from config.flaskconfig import SQLALCHEMY_DATABASE_URI
@@ -54,72 +54,6 @@ def upload_files_to_s3(local_path, s3path):
             logger.info('Data uploaded to %s', {s3path} + '/raw/' + {file})
 
 
-def remove_accents(s):
-    return unicodedata.normalize('NFD', s)
-
-
-def render_text(text, entities):
-    entity_locs = []
-    for ent in entities:
-        if ' (organization)' in ent:
-            ent = ent.replace(' (organization)', '')
-        start = text.find(ent)
-        end = start + len(ent)
-        entity_locs.append({'start': start, 'end': end, 'label': ''})
-
-    colors = {"": "linear-gradient(90deg, #a2dff0, #b1d3ae)"}
-    options = {"ents": [""], "colors": colors}
-
-    doc = [{"text": text,
-            "ents": entity_locs,
-            "title": None}]
-    return displacy.render(doc, style="ent", jupyter=False, manual=True, options=options)
-
-
-def ingest_wiki(wiki_df, engine_string):
-    """ ingest wiki dataframe """
-
-    tm = WikiNewsManager(engine_string=engine_string)
-    for _, row in wiki_df.iterrows():
-        date, news_id, title, wiki, url, image = row
-        tm.add_wiki(date, news_id, title, wiki, url, image)
-    logger.info(f"{len(wiki_df)} rows added to 'wiki' table")
-    tm.close()
-
-
-def ingest_news(news_df, df, engine_string):
-    """ ingest news dataframe """
-
-    tm = WikiNewsManager(engine_string=engine_string)
-    for _, row in news_df.iterrows():
-        date, news_id, news, image, url = row
-        entities = df.loc[df['news_id'] == news_id, 'entity'].values
-        news_dis = render_text(news, entities)
-        tm.add_news(date, news_id, news, news_dis, image, url)
-    logger.info(f"{len(news_df)} rows  added to 'news' table")
-    tm.close()
-
-
-def ingest(file_path, engine_string):
-    """after data is joined and filtered, ingest to database
-
-    Args:
-        df (obj `pandas.DataFrame`): output from filter_algo()
-    """
-    df = pd.read_csv(file_path)
-    df = df.fillna('')
-
-    # when accents are removed, the primary keys may no longer be unique
-    df['title'] = df['title'].apply(remove_accents)
-    df = df.drop_duplicates(['news_id', 'entity', 'title'])
-
-    wiki_df = df[['date', 'news_id', 'title', 'wiki', 'wiki_url', 'wiki_image']].drop_duplicates()
-    ingest_wiki(wiki_df, engine_string)
-
-    news_df = df[['date', 'news_id', 'news', 'news_image', 'news_url']].drop_duplicates()
-    ingest_news(news_df, df, engine_string)
-
-
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Create and/or add data to database")
@@ -135,7 +69,6 @@ if __name__ == '__main__':
     if args.config is not None:
         with open(args.config, "r") as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
-
         logger.info("Configuration file loaded from %s" % args.config)
 
     if args.input is not None:
@@ -163,55 +96,3 @@ if __name__ == '__main__':
     if args.output is not None:
         output.to_csv(args.output, index=False)
         logger.info("Output saved to %s" % args.output)
-
-    # # Add parsers for both creating a database and adding entries to it
-    # parser = argparse.ArgumentParser(description="Create and/or add data to database")
-    # subparsers = parser.add_subparsers(dest='subparser_name')
-
-    # # Sub-parser for creating a database
-    # sb_create = subparsers.add_parser('create_db', description="Create database")
-    # sb_create.add_argument("--engine_string", default='sqlite:///data/entries.db',
-    #                        help="SQLAlchemy connection URI for database")
-
-    # # Sub-parser for ingesting new data
-    # sb_ingest = subparsers.add_parser('ingest', description="Add data to database")
-    # sb_ingest.add_argument("--engine_string", default='sqlite:///data/entries.db',
-    #                        help="SQLAlchemy connection URI for database")
-    # sb_ingest.add_argument("--local_path", default='./data/sample',
-    #                        help="Filepath for local data for database to ingest")
-
-    # # Sub-parser to load new data into local ./data folder
-    # sb_load = subparsers.add_parser('load_new', description="Query new data from APIs and save locally")
-    # sb_load.add_argument('--local_path', default='./data/',
-    #                      help="Load daily local data")
-
-    # # Sub-parser to load data from local path into s3
-    # sb_s3 = subparsers.add_parser('load_s3', description="Describe where local data is")
-    # sb_s3.add_argument('--s3path', default='s3://2021-msia-423-ho-sara',
-    #                    help="If used, will load data to s3")
-    # sb_s3.add_argument('--local_path', default='./data/sample',
-    #                    help="Where to load data to in S3")
-
-    # args = parser.parse_args()
-    # sp_used = args.subparser_name
-
-    # if sp_used == 'create_db':
-    #     create_db(args.engine_string)
-
-    # elif sp_used == 'ingest':
-    #     engine_string = args.engine_string
-    #     df = join_data(args.local_path)
-    #     df = filter_data(df)
-    #     logger.info(df.columns)
-    #     ingest(df, engine_string)
-
-    # elif sp_used == 'load_new':
-    #     # by default, this will save new csv files into ./data
-    #     news = load_news(NEWS_API_KEY, args.local_path)
-    #     load_wiki(news, n_results=1)
-
-    # elif sp_used == 'load_s3':
-    #     upload_files_to_s3(args.local_path, args.s3path)
-
-    # else:
-    #     parser.print_help()
---
+++ src/ingest.py
@@ -0,0 +1,79 @@
+import os
+import unicodedata
+import logging
+import logging.config
+
+import pandas as pd
+from spacy import displacy
+
+from src.add_entries import WikiNewsManager, create_db
+
+logging.config.fileConfig("config/logging/local.conf",
+                          disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+def remove_accents(s):
+    return unicodedata.normalize('NFD', s)
+
+
+def render_text(text, entities):
+    entity_locs = []
+    for ent in entities:
+        if ' (organization)' in ent:
+            ent = ent.replace(' (organization)', '')
+        start = text.find(ent)
+        end = start + len(ent)
+        entity_locs.append({'start': start, 'end': end, 'label': ''})
+
+    colors = {"": "linear-gradient(90deg, #a2dff0, #b1d3ae)"}
+    options = {"ents": [""], "colors": colors}
+
+    doc = [{"text": text,
+            "ents": entity_locs,
+            "title": None}]
+    return displacy.render(doc, style="ent", jupyter=False, manual=True, options=options)
+
+
+def ingest_wiki(wiki_df, engine_string):
+    """ ingest wiki dataframe """
+
+    tm = WikiNewsManager(engine_string=engine_string)
+    for _, row in wiki_df.iterrows():
+        date, news_id, title, wiki, url, image = row
+        tm.add_wiki(date, news_id, title, wiki, url, image)
+    logger.info(f"{len(wiki_df)} rows added to 'wiki' table")
+    tm.close()
+
+
+def ingest_news(news_df, df, engine_string):
+    """ ingest news dataframe """
+
+    tm = WikiNewsManager(engine_string=engine_string)
+    for _, row in news_df.iterrows():
+        date, news_id, news, image, url = row
+        entities = df.loc[df['news_id'] == news_id, 'entity'].values
+        news_dis = render_text(news, entities)
+        tm.add_news(date, news_id, news, news_dis, image, url)
+    logger.info(f"{len(news_df)} rows  added to 'news' table")
+    tm.close()
+
+
+def ingest(file_path, engine_string):
+    """after data is joined and filtered, ingest to database
+
+    Args:
+        df (obj `pandas.DataFrame`): output from filter_algo()
+    """
+    df = pd.read_csv(file_path)
+    df = df.fillna('')
+
+    # when accents are removed, the primary keys may no longer be unique
+    df['title'] = df['title'].apply(remove_accents)
+    df = df.drop_duplicates(['news_id', 'entity', 'title'])
+
+    wiki_df = df[['date', 'news_id', 'title', 'wiki', 'wiki_url', 'wiki_image']].drop_duplicates()
+    ingest_wiki(wiki_df, engine_string)
+
+    news_df = df[['date', 'news_id', 'news', 'news_image', 'news_url']].drop_duplicates()
+    ingest_news(news_df, df, engine_string)
