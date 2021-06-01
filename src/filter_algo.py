c615f41fcf0b254b00b1d427d6d605a58d209c7a
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-01 00:27:11 -0500

    major improvements; correct column names and remove reliance on filename

--- src/filter_algo.py
+++ src/filter_algo.py
@@ -1,9 +1,12 @@
 import os
+from datetime import date
 
-import nltk
 from difflib import SequenceMatcher
-from nltk.corpus import stopwords
 import pandas as pd
+import nltk
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 
 def sim_sm(x):
@@ -22,10 +25,8 @@ def remove_stopwords(df):
     stop_words = stopwords.words('english')
     stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
 
-    df['wiki'] = df['wiki'].str.lower().str.split()
-    df['news'] = df['news'].str.lower().str.split()
-    df['wiki'] = df['wiki'].apply(lambda x: ' '.join([item for item in x if item not in stop_words]))
-    df['news'] = df['news'].apply(lambda x: ' '.join([item for item in x if item not in stop_words]))
+    df['wiki_process'] = df['wiki'].str.lower().str.split().apply(lambda x: ' '.join([item for item in x if item not in stop_words]))
+    df['news_process'] = df['news'].str.lower().str.split().apply(lambda x: ' '.join([item for item in x if item not in stop_words]))
     return df
 
 
@@ -38,42 +39,33 @@ def join_data(datadir):
         if 'wiki' in file:
             df = pd.read_csv(datadir + '/' + file)
 
-            # add date column
-            date = '-'.join(file.split('-')[0:3])
-            df['date'] = date
-
             # remove duplicates
-            df.drop_duplicates(['title', 'news_id', 'date'])
+            df.drop_duplicates(['title', 'news_id'])
 
-            # set index
+            # create index / primary key
             df = df.reset_index()
             df.rename(columns={'index': 'wiki_id'}, inplace=True)
             wikidf.append(df)
 
         elif 'news' in file:
             df = pd.read_csv(datadir + '/' + file)
-            date = '-'.join(file.split('-')[0:3])
-            df['date'] = date
             newsdf.append(df)
 
-    data = pd.concat(wikidf).merge(pd.concat(newsdf), on=['news_id', 'date'])
-    print(data.columns)
-    data = data.sort_values(['date', 'news_id', 'wiki_id'])
-    return data
+    joined = pd.concat(wikidf).merge(pd.concat(newsdf), on=['news_id'])
+    joined['date'] = date.today().strftime("%b-%d-%Y")
+    joined = joined.sort_values(['news_id', 'wiki_id'])
+    return joined
 
 
-def filter_data(df):
+def filter_data(datadir):
+
+    df = join_data(datadir)
+
     df = df.dropna()
     df = remove_stopwords(df)
 
-    df['sm_sim'] = df[['wiki', 'news']].apply(sim_sm, axis=1)
+    df['sm_sim'] = df[['wiki_process', 'news_process']].apply(sim_sm, axis=1)
     df['predict'] = df['sm_sim'] > 0.2
     df.drop_duplicates(['news_id', 'title']).loc[df['predict']]
 
     return df
-
-
-if __name__ == '__main__':
-
-    df = join_data('./data/sample')
-    filter_data(df).to_csv('./data/sample/May-29-2021-joined.csv', index=False)
