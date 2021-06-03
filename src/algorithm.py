c3e00132c12a5a860c636259a8adc04d11ab96a7
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-02 22:19:03 -0500

    add docstrings and yaml

--- src/algorithm.py
+++ src/algorithm.py
@@ -13,80 +13,81 @@ def sim_sm(x):
     """Calculate the SequenceMatcher similarity score
 
     Args:
-        x (array-like): containing two strings to compares
+        x (array-like): containing two strings to compare
 
     Returns:
-        float: similarity score
+        (float): similarity score
     """
     return SequenceMatcher(None, x[0], x[1]).ratio()
 
 
-def remove_stopwords(df):
-    """[summary]
+def remove_stopwords(df, args):
+    """remove stopwords from specified dataframe columns
 
     Args:
-        df ([type]): [description]
+        df (pandas dataframe): dataframe with some str columns
+        args (dict): dict with 'config': yaml file path
 
     Returns:
-        [type]: [description]
+        (pandas dataframe): dataframe, but with processed str
+                               columns with stopwords removed
     """
     stop_words = stopwords.words('english')
-    stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
 
-    df['wiki_process'] = df['wiki'].str.lower().str.split().apply(lambda x: ' '.join([item for item in x if item not in stop_words]))
-    df['news_process'] = df['news'].str.lower().str.split().apply(lambda x: ' '.join([item for item in x if item not in stop_words]))
+    with open(args.config, 'r') as f:
+        c = yaml.load(f, Loader=yaml.FullLoader)
+
+    for raw, processed in zip(c['raw_features'], c['processed_features']):
+        df[processed] = df[raw].str.lower().str.split(). \
+            apply(lambda x: ' '.join([item for item in x
+                                      if item not in stop_words]))
+
     return df
 
 
 def join_data(news_path, wiki_path):
-    """[summary]
+    """Join news file with wiki file
 
     Args:
-        datadir ([type]): [description]
+        news_path (str): file path
+        wiki_path (str): file path
 
     Returns:
-        [type]: [description]
+        (pandas dataframe): joined dataframe
     """
-    wikidf = []
-    newsdf = []
 
-    # if 'wiki' in file:
-    df = pd.read_csv(wiki_path)
+    wikidf = pd.read_csv(wiki_path)
+    wikidf.drop_duplicates(['title', 'news_id'])
+    wikidf.reset_index(inplace=True)
+    wikidf.rename(columns={'index': 'wiki_id'}, inplace=True)
 
-    # remove duplicates
-    df.drop_duplicates(['title', 'news_id'])
+    newsdf = pd.read_csv(news_path)
 
-    # create index / primary key
-    df = df.reset_index()
-    df.rename(columns={'index': 'wiki_id'}, inplace=True)
-    wikidf.append(df)
-
-    # elif 'news' in file:
-    df = pd.read_csv(news_path)
-    newsdf.append(df)
-
-    joined = pd.concat(wikidf).merge(pd.concat(newsdf), on=['news_id'])
+    joined = wikidf.merge(newsdf, on=['news_id'])
     joined['date'] = date.today().strftime("%b-%d-%Y")
-    joined = joined.sort_values(['news_id', 'wiki_id'])
     return joined
 
 
-def filter_data(join_path):
-    """[summary]
+def filter_data(args):
+    """Filter data based on arguments
 
     Args:
-        datadir ([type]): [description]
+        args (dict): dict with 'config': yaml file path
+                     and 'input': file path of joined() ouput
 
     Returns:
-        [type]: [description]
+        (pandas dataframe): data with irrelevant entities removed
     """
-    df = pd.read_csv(join_path)
+    with open(args.config, 'r') as f:
+        c = yaml.load(f, Loader=yaml.FullLoader)
+
+    df = pd.read_csv(args.input)
 
     df = df.dropna()
     df = remove_stopwords(df)
 
-    df['sm_sim'] = df[['wiki_process', 'news_process']].apply(sim_sm, axis=1)
-    df['predict'] = df['sm_sim'] > 0.2
+    df['sm_sim'] = df[c['processed_features']].apply(sim_sm, axis=1)
+    df['predict'] = df['sm_sim'] > c['threshhold']
     df.drop_duplicates(['news_id', 'title']).loc[df['predict']]
 
     return df
