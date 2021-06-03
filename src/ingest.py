552e9ade40e278a5c0f7c47f6a57ab2edba369e9
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-02 19:04:13 -0500

    convert ingest code to class

--- src/ingest.py
+++ src/ingest.py
@@ -1,74 +1,76 @@
 import os
 import unicodedata
 import logging
-import logging.config
 
 import pandas as pd
-from spacy import displacy
 
 from src.db import WikiNewsManager, create_db
 
-logging.config.fileConfig("config/logging/local.conf",
-                          disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 
-def remove_accents(s):
-    """ remove accents which certain databases may not be able to handle """
-    return unicodedata.normalize('NFD', s)
+class IngestManager:
 
+    def __init__(self, file_path=None, engine_string=None):
+        self.file_path = file_path
+        self.engine_string = engine_string
 
-def render_text(text, entities):
-    """ custom rendering of text with highlighted terms """
-    entity_locs = []
-    for ent in entities:
-        if ' (organization)' in ent:
-            ent = ent.replace(' (organization)', '')
-        text = text.replace(ent, f'<span class="highlight">{ent}</span>')
-    return text
+        logger.info('init IngestManager')
 
+    def __remove_accents(self, s):
+        """ remove accents which database may not be able to handle """
+        return unicodedata.normalize('NFD', s)
 
-def ingest_wiki(wiki_df, engine_string):
-    """ ingest wiki dataframe """
+    def __render_text(self, text, entities):
+        """ custom rendering of text with highlighted terms """
+        entity_locs = []
+        for ent in entities:
+            if ' (organization)' in ent:
+                ent = ent.replace(' (organization)', '')
+            text = text.replace(ent, f'<span class="highlight">{ent}</span>')
+        return text
 
-    tm = WikiNewsManager(engine_string=engine_string)
-    for _, row in wiki_df.iterrows():
-        date, news_id, title, wiki, url, image = row
-        tm.add_wiki(date, news_id, title, wiki, url, image)
-    logger.info(f"{len(wiki_df)} rows added to 'wiki' table")
-    tm.close()
+    def __ingest_wiki(self, wiki_df):
+        """ ingest wiki dataframe """
 
+        tm = WikiNewsManager(engine_string=self.engine_string)
+        for _, row in wiki_df.iterrows():
+            date, news_id, title, wiki, url, image = row
+            tm.add_wiki(date, news_id, title, wiki, url, image)
+        logger.info(f"{len(wiki_df)} rows added to 'wiki' table")
+        tm.close()
 
-def ingest_news(news_df, df, engine_string):
-    """ ingest news dataframe """
+    def __ingest_news(self, news_df, df):
+        """ ingest news dataframe """
 
-    tm = WikiNewsManager(engine_string=engine_string)
-    for _, row in news_df.iterrows():
-        date, news_id, headline, news, image, url = row
+        tm = WikiNewsManager(engine_string=self.engine_string)
+        for _, row in news_df.iterrows():
+            date, news_id, headline, news, image, url = row
 
-        entities = df.loc[df['news_id'] == news_id, 'entity'].drop_duplicates().values
-        news_dis = render_text(news, entities)
+            entities = df.loc[df['news_id'] == news_id, 'entity']. \
+                drop_duplicates().values
+            news_dis = self.__render_text(news, entities)
 
-        tm.add_news(date, news_id, headline, news, news_dis, image, url)
-    logger.info(f"{len(news_df)} rows  added to 'news' table")
-    tm.close()
+            tm.add_news(date, news_id, headline, news, news_dis, image, url)
+        logger.info(f"{len(news_df)} rows  added to 'news' table")
+        tm.close()
 
+    def ingest(self):
+        """after data is joined and filtered, ingest to database
 
-def ingest(file_path, engine_string):
-    """after data is joined and filtered, ingest to database
+        Args:
+            file_path (str): file_path referencing output from filter_algo()
+            enging_string (str): file_path referencing output from filter_algo()
+        """
+        df = pd.read_csv(self.file_path)
+        df = df.fillna('')
 
-    Args:
-        df (obj `pandas.DataFrame`): output from filter_algo()
-    """
-    df = pd.read_csv(file_path)
-    df = df.fillna('')
+        # when accents are removed, the primary keys may no longer be unique
+        df['title'] = df['title'].apply(self.__remove_accents)
+        df = df.drop_duplicates(['news_id', 'entity', 'title'])
 
-    # when accents are removed, the primary keys may no longer be unique
-    df['title'] = df['title'].apply(remove_accents)
-    df = df.drop_duplicates(['news_id', 'entity', 'title'])
+        wiki_df = df[['date', 'news_id', 'title', 'wiki', 'wiki_url', 'wiki_image']].drop_duplicates()
+        self.__ingest_wiki(wiki_df)
 
-    wiki_df = df[['date', 'news_id', 'title', 'wiki', 'wiki_url', 'wiki_image']].drop_duplicates()
-    ingest_wiki(wiki_df, engine_string)
-
-    news_df = df[['date', 'news_id', 'headline', 'news', 'news_image', 'news_url']].drop_duplicates()
-    ingest_news(news_df, df, engine_string)
+        news_df = df[['date', 'news_id', 'headline', 'news', 'news_image', 'news_url']].drop_duplicates()
+        self.__ingest_news(news_df, df)
