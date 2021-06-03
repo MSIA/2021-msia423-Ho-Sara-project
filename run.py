b7cf846de9b9e7ae8324f13a8f85b789fec73982
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-02 19:04:49 -0500

    modularize s3 function

--- run.py
+++ run.py
@@ -5,57 +5,34 @@ import logging.config
 import yaml
 
 import pandas as pd
-import boto3
-import botocore
-import spacy
 
 from src.load import load_wiki, load_news
-from src.ingest import *
 from src.algorithm import filter_data, join_data
+from src.db import create_db
+from src.ingest import IngestManager
 
 from config.flaskconfig import SQLALCHEMY_DATABASE_URI
 
 NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
-logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', level=logging.DEBUG)
-logging.getLogger("botocore").setLevel(logging.ERROR)
-logging.getLogger("s3transfer").setLevel(logging.ERROR)
-logging.getLogger("urllib3").setLevel(logging.ERROR)
-logging.getLogger("boto3").setLevel(logging.ERROR)
-logging.getLogger("asyncio").setLevel(logging.ERROR)
-logging.getLogger("aiobotocore").setLevel(logging.ERROR)
-logging.getLogger("s3fs").setLevel(logging.ERROR)
-
 logging.config.fileConfig("config/logging/local.conf",
                           disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-
-def upload_files_to_s3(local_path, s3path):
-    """ Upload local files to s3 """
-
-    s3bucket = s3path.replace('s3://', '')
-
-    s3 = boto3.resource("s3")
-    bucket = s3.Bucket(s3bucket)
-
-    files = os.listdir(local_path)
-    files = [file for file in files if '.csv' in file]
-    for file in files:
-        try:
-            bucket.upload_file(f'{local_path}/{file}', f'raw/{file}')
-        except botocore.exceptions.NoCredentialsError:
-            logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
-        else:
-            logger.info('Data uploaded to %s', {s3path} + '/raw/' + {file})
+with open('config/config.yaml', "r") as f:
+    c = yaml.load(f, Loader=yaml.FullLoader)
+print(c)
+logger.info("Configuration file loaded from %s" % 'config/config.yaml')
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="Create and/or add data to database")
-    parser.add_argument('step', 
-                        help='Which step to run', 
-                        choices=['load_news', 'load_wiki', 'filter', 'create_db', 'join', 'ingest'])
+    parser = argparse.ArgumentParser(
+        description="Create and/or add data to database")
+    parser.add_argument('step',
+                        help='Which step to run',
+                        choices=['load_news', 'load_wiki', 'filter',
+                                 'create_db', 'join', 'ingest'])
 
     parser.add_argument('--input', '-i', default=None, 
                         help='Path to input data')
@@ -66,16 +43,12 @@ if __name__ == '__main__':
 
     parser.add_argument('--config', help='Path to configuration file')
     parser.add_argument('--output', '-o', default=None,
-                        help='Path to save output CSV (optional, default = None)')
-    parser.add_argument("--engine_string", default='sqlite:///data/entries.db',
-                        help="SQLAlchemy connection URI for database")
+                        help='Path to save output CSV (default = None)')
+
+    parser.add_argument("--engine_string", default=SQLALCHEMY_DATABASE_URI,
+                        help="connection URI for database")
 
     args = parser.parse_args()
-
-    if args.config is not None:
-        with open(args.config, "r") as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-        logger.info("Configuration file loaded from %s" % args.config)
 
     if args.input is not None:
         input = pd.read_csv(args.input)
@@ -85,16 +58,20 @@ if __name__ == '__main__':
         output = load_news(NEWS_API_KEY)
 
     elif args.step == 'load_wiki':
-        output = load_wiki(args.input, n_results=1)
+        output = load_wiki(args.input,
+                           n_results=c['n_results'])
 
     elif args.step == 'create_db':
         create_db(args.engine_string)
 
     elif args.step == 'join':
-        output = join_data(args.input1, args.input2)
+        output = join_data(args.input1,
+                           args.input2)
 
     elif args.step == 'ingest':
-        ingest(args.input, args.engine_string)
+        ing = IngestManager(file_path=args.input,
+                            engine_string=args.engine_string)
+        ing.ingest()
 
     # else:
     #     test.run_tests(args)
---
+++ src/s3.py
@@ -0,0 +1,32 @@
+import os
+
+import boto3
+import botocore
+
+logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', level=logging.DEBUG)
+logging.getLogger("botocore").setLevel(logging.ERROR)
+logging.getLogger("s3transfer").setLevel(logging.ERROR)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+logging.getLogger("boto3").setLevel(logging.ERROR)
+logging.getLogger("asyncio").setLevel(logging.ERROR)
+logging.getLogger("aiobotocore").setLevel(logging.ERROR)
+logging.getLogger("s3fs").setLevel(logging.ERROR)
+
+
+def upload_files_to_s3(local_path, s3path):
+    """ Upload local files to s3 """
+
+    s3bucket = s3path.replace('s3://', '')
+
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(s3bucket)
+
+    files = os.listdir(local_path)
+    files = [file for file in files if '.csv' in file]
+    for file in files:
+        try:
+            bucket.upload_file(f'{local_path}/{file}', f'raw/{file}')
+        except botocore.exceptions.NoCredentialsError:
+            logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
+        else:
+            logger.info('Data uploaded to %s', {s3path} + '/raw/' + {file})
