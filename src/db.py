451b0e2bff0fd3683ffd28b2120304729db92dd4
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-03 23:56:44 -0500

    move ingest script to db.py

--- src/db.py
+++ src/db.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import logging
 import logging.config
 
+import pandas as pd
+from unidecode import unidecode
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, MetaData
@@ -84,9 +86,11 @@ class WikiNewsManager:
             engine_string: str - Engine string
         """
         if app:
+            logger.info('using WikiNewsManager for app')
             self.db = SQLAlchemy(app)
             self.session = self.db.session
         if engine_string:
+            logger.info('using WikiNewsManager for db')
             engine = sqlalchemy.create_engine(engine_string)
             Session = sessionmaker(bind=engine)
             self.session = Session()
@@ -134,5 +138,78 @@ class WikiNewsManager:
 
         session.add(wiki_record)
         session.commit()
-        logger.debug(f"'{title}' added to database corresponding ",
-                     "to news_id {str(news_id)}")
+        logger.debug(f"'{title}' added to db ~ for news_id {str(news_id)}")
+
+
+def remove_accents(s):
+    """ remove accents which database may not be able to handle """
+    return unidecode(s)
+
+
+def render_text(text, entities):
+    """ custom rendering of text with highlighted terms """
+    entity_locs = []
+    for ent in entities:
+        if ' (organization)' in ent:
+            ent = ent.replace(' (organization)', '')
+        text = text.replace(ent, f'<span class="highlight">{ent}</span>')
+    return text
+
+
+def ingest_wiki(wiki_df, engine_string):
+    """ ingest wiki dataframe """
+
+    tm = WikiNewsManager(app=None, engine_string=engine_string)
+    for _, row in wiki_df.iterrows():
+        date, news_id, title, wiki, url, image = row
+        tm.add_wiki(date, news_id, title, wiki, url, image)
+    logger.info(f"{len(wiki_df)} rows added to 'wiki' table")
+    tm.close()
+
+
+def render_news_col(news_df, df):
+    """ ingest news dataframe """
+    news_df['news_dis'] = ''
+    news_obs = news_df['news'].unique()
+
+    for i, row in news_df.iterrows():
+        date, news_id, headline, news, image, url, _ = row
+        entities = df.loc[df['news_id'] == news_id, 'entity']. \
+            drop_duplicates().values
+        news_df.loc[i, 'news_dis'] = render_text(news, entities)
+    return news_df
+
+
+def ingest_news(news_df, engine_string):
+    """ ingest news dataframe """
+
+    tm = WikiNewsManager(app=None, engine_string=engine_string)
+    for _, row in news_df.iterrows():
+        date, news_id, headline, news, image, url, news_dis = row
+        tm.add_news(date, news_id, headline, news, news_dis, image, url)
+    logger.info(f"{len(news_df)} rows  added to 'news' table")
+    tm.close()
+
+
+def ingest(file_path, engine_string):
+    """after data is joined and filtered, ingest to database
+
+    Args:
+        file_path (str): file_path referencing output from filter_algo()
+        enging_string (str): file_path referencing output from filter_algo()
+    """
+    df = pd.read_csv(file_path)
+    df = df.fillna('')
+
+    # when accents are removed, the primary keys may no longer be unique
+    df['title'] = df['title'].apply(remove_accents)
+    df = df.drop_duplicates(['news_id', 'entity', 'title'])
+
+    wiki_df = df[['date', 'news_id', 'title', 'wiki', 'wiki_url', 'wiki_image']] \
+        .drop_duplicates()
+    ingest_wiki(wiki_df, engine_string)
+
+    news_df = df[['date', 'news_id', 'headline', 'news', 'news_image', 'news_url']] \
+        .drop_duplicates()
+    news_df = render_news_col(news_df, df)
+    ingest_news(news_df, engine_string)
