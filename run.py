5e91ce8688c1619396df171055d4b662fe25ed99
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-05 01:29:03 -0500

    added s3 to the pipeline

--- run.py
+++ run.py
@@ -5,23 +5,23 @@ import logging.config
 import yaml
 
 import pandas as pd
+import s3fs
 
+from src.db import WikiNewsManager, create_db, ingest
 from src.load import load_wiki, load_news
 from src.algorithm import filter_data, join_data
-from src.db import create_db
-from src.ingest import IngestManager
+from src.s3 import upload
 
 from config.flaskconfig import SQLALCHEMY_DATABASE_URI
-
-NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 logging.config.fileConfig("config/logging/local.conf",
                           disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
+logging.getLogger("s3fs").setLevel(logging.WARNING)
+
 with open('config/config.yaml', "r") as f:
     c = yaml.load(f, Loader=yaml.FullLoader)
-print(c)
 logger.info("Configuration file loaded from %s" % 'config/config.yaml')
 
 
@@ -32,13 +32,13 @@ if __name__ == '__main__':
     parser.add_argument('step',
                         help='Which step to run',
                         choices=['load_news', 'load_wiki', 'filter',
-                                 'create_db', 'join', 'ingest'])
+                                 'create_db', 'join', 'ingest', 's3'])
 
-    parser.add_argument('--input', '-i', default=None, 
+    parser.add_argument('--input', '-i', default=None,
                         help='Path to input data')
-    parser.add_argument('--input1', default=None, 
+    parser.add_argument('--input1', default=None,
                         help='Path to input data')
-    parser.add_argument('--input2', default=None, 
+    parser.add_argument('--input2', default=None,
                         help='Path to input data')
 
     parser.add_argument('--config', help='Path to configuration file')
@@ -47,35 +47,55 @@ if __name__ == '__main__':
 
     parser.add_argument("--engine_string", default=SQLALCHEMY_DATABASE_URI,
                         help="connection URI for database")
+    parser.add_argument("--s3_path",
+                        help="s3 path")
 
     args = parser.parse_args()
 
-    if args.input is not None:
-        input = pd.read_csv(args.input)
-        logger.info('Input data loaded from %s', args.input)
-
     if args.step == 'load_news':
-        output = load_news(NEWS_API_KEY)
+        output = load_news(args)
 
     elif args.step == 'load_wiki':
-        output = load_wiki(args.input,
-                           n_results=c['n_results'])
+        output = load_wiki(args)
 
     elif args.step == 'create_db':
         create_db(args.engine_string)
 
     elif args.step == 'join':
+        if args.s3_path is not None:
+            logger.info('Using s3 path')
+            args.input1 = args.s3_path + '/' + args.input1
+            args.input2 = args.s3_path + '/' + args.input2
+
         output = join_data(args.input1,
                            args.input2)
 
-    elif args.step == 'ingest':
-        ing = IngestManager(file_path=args.input,
-                            engine_string=args.engine_string)
-        ing.ingest()
+    elif args.step == 'filter':
+        if args.s3_path is not None:
+            logger.info('Using s3 path for filtering')
+            args.input = args.s3_path + '/' + args.input
 
-    # else:
-    #     test.run_tests(args)
+        output = filter_data(args)
+
+    elif args.step == 'ingest':
+        if args.s3_path is not None:
+            logger.info('Using s3 path for ingesting')
+            args.input = args.s3_path + '/' + args.input
+
+        ingest(file_path=args.input,
+               engine_string=args.engine_string)
+
+    elif args.step == 's3':
+        if args.input is not None:
+            upload(args.input, args.s3_path)
+        if args.input1 is not None:
+            upload(args.input1, args.s3_path)
+        if args.input2 is not None:
+            upload(args.input2, args.s3_path)
 
     if args.output is not None:
         output.to_csv(args.output, index=False)
         logger.info("Output saved to %s" % args.output)
+
+        if args.s3_path is not None:
+            upload(args.output, args.s3_path)
--- src/s3.py
+++ src/s3.py
@@ -16,30 +16,18 @@ logging.getLogger("s3fs").setLevel(logging.ERROR)
 logger = logging.getLogger(__name__)
 
 
-def upload(local_path, s3path):
+def upload(local_file, s3_path):
     """ Upload local files to s3 """
 
-    s3bucket = s3path.replace('s3://', '')
+    s3bucket = s3_path.replace('s3://', '')
 
     s3 = boto3.resource("s3")
     bucket = s3.Bucket(s3bucket)
 
-    files = os.listdir(local_path)
-    files = [file for file in files if '.csv' in file]
-    for file in files:
-        try:
-            bucket.upload_file(f'{local_path}/{file}', f'raw/{file}')
-        except botocore.exceptions.NoCredentialsError:
-            logger.error('Please provide credentials via AWS_ACCESS_KEY_ID ',
-                         'and AWS_SECRET_ACCESS_KEY env variables')
-        else:
-            logger.info('Data uploaded to %s', {s3path} + '/raw/' + {file})
-
-
-def s3_static():
-    upload('data/labeled', s3path)
-    upload('data/sample', s3path)
-
-
-def s3_dynamic():
-    upload(, s3path)
+    try:
+        bucket.upload_file(f'{local_file}', f'{local_file}')
+    except botocore.exceptions.NoCredentialsError:
+        logger.error('Please provide credentials via AWS_ACCESS_KEY_ID ',
+                     'and AWS_SECRET_ACCESS_KEY env variables')
+    else:
+        logger.info('Data uploaded to s3 path %s', local_file)
