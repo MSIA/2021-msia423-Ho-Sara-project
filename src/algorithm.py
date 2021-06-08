a0eb28e933f733fab997417df224a87e164e6803
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-07 23:23:39 -0500

    add docstrings

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,69 +1,28 @@
 """ Module containing functions to format the data for the algorithm and run it
 
+join_data(news_path, wiki_path)
+filter_data(data, conf)
+
+helper functions:
+    text_to_vector(text)
+    get_cosine(x)
+    remove_stopwords(data, args)
+
 """
 
 from datetime import date
-from difflib import SequenceMatcher
 import logging
 import math
 import re
 from collections import Counter
 
-import yaml
 import pandas as pd
-from scipy import stats
 
 from nltk.corpus import stopwords
 
 logger = logging.getLogger(__name__)
 
 logging.getLogger("utils").setLevel(logging.ERROR)
-
-
-def text_to_vector(text):
-    word = re.compile(r"\w+")
-    words = word.findall(text)
-    return Counter(words)
-
-
-def get_cosine(x):
-    vec1 = text_to_vector(x[0])
-    vec2 = text_to_vector(x[1])
-
-    intersection = set(vec1.keys()) & set(vec2.keys())
-    numerator = sum([vec1[x] * vec2[x] for x in intersection])
-
-    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
-    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
-    denominator = math.sqrt(sum1) * math.sqrt(sum2)
-
-    if not denominator:
-        return 0.0
-    else:
-        return float(numerator) / denominator
-
-
-def remove_stopwords(df, args):
-    """remove stopwords from specified dataframe columns
-
-    Args:
-        df (obj `pandas.DataFrame`): dataframe with some str columns
-        args (dict): yaml-style config with keys
-            'raw_features' and 'processed_features'
-
-    Returns:
-        (obj `pandas.DataFrame`): dataframe, but with a processed str
-                                  column with stopwords removed
-    """
-    stop_words = stopwords.words('english')
-
-    for raw, proc in zip(args['raw_features'], args['processed_features']):
-        df[proc] = df[raw].str.lower().str.split(). \
-            apply(lambda x: ' '.join([item for item in x
-                                      if item not in stop_words]))
-
-    logger.debug("removed stopwords, returning processed df")
-    return df
 
 
 def join_data(news_path, wiki_path):
@@ -89,38 +48,75 @@ def join_data(news_path, wiki_path):
     return joined
 
 
-def filter_data(args):
+def filter_data(data, conf):
     """Orchestration function; clean and filter based on similarity score
 
     Args:
-        args (dict): yaml-style config with keys:
-            'config': yaml file path
-            'input': file path of joined() ouput
+        data (obj `pandas.DataFrame`): output from join_data()
+        conf (dict): yaml-style config with keys:
             'processed_features': to be processed and used for similarity score
             'threshhold': similarity score cutoff to determine relevancy
 
     Returns:
         (obj `pandas.DataFrame`): data with irrelevant entities removed
     """
-    with open(args.config, 'r') as conf_file:
-        conf = yaml.load(conf_file, Loader=yaml.FullLoader)
-        logger.debug("loaded yaml from : %s", args.config)
 
-    df = pd.read_csv(args.input)
-    logger.debug('read %i lines of data', len(df))
-
-    df = remove_stopwords(df, conf)
+    data = remove_stopwords(data, conf)
     logger.info('removed stop words')
 
-    df['sm_sim'] = df[conf['processed_features']].apply(cos_pipeline, axis=1)
-    logger.info("mean similarity score: %f", df['sm_sim'].mean())
+    data['sim'] = data[conf['processed_features']].apply(get_cosine, axis=1)
+    logger.info("mean similarity score: %f", data['sm_sim'].mean())
 
-    df['predict'] = df['sm_sim'] > conf['threshhold']
-    df = df.drop_duplicates(['news_id', 'title']).loc[df['predict']]
+    data['predict'] = data['sim'] > conf['threshhold']
+    data = data.drop_duplicates(['news_id', 'title']).loc[data['predict']]
 
-    return df
+    return data
 
 
-def eval_(args):
-    args.input()
+def text_to_vector(text):
+    """embed text based on counter"""
 
+    word = re.compile(r"\w+")
+    words = word.findall(text)
+    return Counter(words)
+
+
+def get_cosine(x):
+    """calculate cosine of text-embedded vectors"""
+
+    vec1 = text_to_vector(x[0])
+    vec2 = text_to_vector(x[1])
+
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
+    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    return float(numerator) / denominator
+
+
+def remove_stopwords(data, args):
+    """remove stopwords from specified dataframe columns
+
+    Args:
+        data (obj `pandas.DataFrame`): dataframe with some str columns
+        args (dict): yaml-style config with keys
+            'raw_features' and 'processed_features'
+
+    Returns:
+        (obj `pandas.DataFrame`): dataframe, but with a processed str
+                                  column with stopwords removed
+    """
+    stop_words = stopwords.words('english')
+
+    for raw, proc in zip(args['raw_features'], args['processed_features']):
+        data[proc] = data[raw].str.lower().str.split(). \
+            apply(lambda x: ' '.join([item for item in x
+                                      if item not in stop_words]))
+
+    logger.debug("removed stopwords, returning processed data")
+    return data
