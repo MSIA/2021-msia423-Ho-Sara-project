47e925d0977e93dbb5ced748dfaaecd5cd9f693a
sara-shiho <saraho2021@u.northwestern.edu>
2021-06-09 22:01:22 -0500

    remove; no longer needed with new Makefile

--- sample.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-date=$(date +"%m-%d-%y")
-
-cp data/sample/news-entries.csv data/daily/${date}-news-entries.csv
-cp data/sample/wiki-entries.csv data/daily/${date}-wiki-entries.csv
